516f3368ae7640ed647e0cfa29ce0e5b2a75cfa1
Robert Kaye <rob@musicbrainz.org>
2014-08-07 13:41:23 +0200

    Added error handling in case there is no match in mbspotify.

--- userscript/mb_spotify.user.js
+++ userscript/mb_spotify.user.js
@@ -10,10 +10,14 @@
 //
 // ==/UserScript==
 
+
 function injected() 
 {
     var spotify_html_begin = '<iframe src="https://embed.spotify.com/?uri=';
-    var spotify_html_end = '" width="218" height="80" frameborder="0" allowtransparency="true"></iframe>';
+    var spotify_html_end = '" width="218" height="80" style="margin-top: 10px" frameborder="0" allowtransparency="true"></iframe>';
+    var no_match_html_begin = '<div width="218" height="80" style="margin-top: 5px; padding: 3px; background-color: #CCC">This release-group has not been matched to Spotify. Please <a href="http://critiquebrainz.org/matching/spotify/';
+    var no_match_html_end = '">match this release group</a>.</div>';
+    var error_html = '<div width="218" height="80" style="margin-top: 5px; padding: 3px; background-color: #CCC">An error has occurred looking up the release-group match.</div>';
 
     mbid = window.location.pathname.substr(15);
     $.ajax({
@@ -24,11 +28,14 @@ function injected()
              contentType: "application/json; charset=utf-8",
              success: function(json)
              {
-                 console.log(spotify_html_begin + json[mbid] + spotify_html_begin);
-                 $(".cover-art").append(spotify_html_begin + json[mbid] + spotify_html_end);
+                 if (json[mbid])
+                     $(".cover-art").append(spotify_html_begin + json[mbid] + spotify_html_end);
+                 else
+                     $(".cover-art").append(no_match_html_begin + mbid + no_match_html_end);
              },
              error: function(xmlhttp, textStatus, error)
              {
+                 $(".cover-art").append(error_html);
              }
     });
 }
