26fbfc5414c1abb65a1d2b8d6994d38b9938f777
Bimalkant Lauhny <code-master5@users.noreply.github.com>
2019-01-20 03:18:20 +0530

    Sleep for consul config (#38)
    * Sleep for consul
    
    * Wait for consul config
    
    * Remove logger
    
    * Remove config file error

--- mbspotify/__init__.py
+++ mbspotify/__init__.py
@@ -1,6 +1,10 @@
 from brainzutils.flask import CustomFlask
 import os
+import sys
+from time import sleep
 
+deploy_env = os.environ.get('DEPLOY_ENV', '')
+CONSUL_CONFIG_FILE_RETRY_COUNT = 10
 
 def create_app(config_path=None):
     app = CustomFlask(
@@ -13,17 +17,31 @@ def create_app(config_path=None):
         os.path.dirname(os.path.realpath(__file__)),
         "..", "default_config.py"
     ))
-    app.config.from_pyfile(os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        '..', 'consul_config.py'
-    ), silent=True)
+
     app.config.from_pyfile(os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         '..', 'custom_config.py'
     ), silent=True)
+
+    if deploy_env:
+        config_file = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            '..', 'consul_config.py'
+        )
+        print("Checking if consul template generated config file exists: {}".format(config_file))
+        for _ in range(CONSUL_CONFIG_FILE_RETRY_COUNT):
+            if not os.path.exists(config_file):
+                sleep(1)
+
+        if not os.path.exists(config_file):
+            print("No configuration file generated yet. Retried {} times, exiting.".format(CONSUL_CONFIG_FILE_RETRY_COUNT))
+            sys.exit(-1)
+
+        print("Loading consul config file {}".format(config_file))
+        app.config.from_pyfile(config_file, silent=True)
+
     if config_path:
         app.config.from_pyfile(config_path)
-
     app.init_loggers(
         file_config=app.config.get("LOG_FILE"),
         sentry_config=app.config.get("LOG_SENTRY"),
