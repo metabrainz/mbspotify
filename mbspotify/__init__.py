465ef696f88b6064ae93ce531ebabceb9f03c8d6
code-master5 <lauhny.bimalk@gmail.com>
2019-01-20 03:01:12 +0530

    Sleep for consul

--- mbspotify/__init__.py
+++ mbspotify/__init__.py
@@ -1,7 +1,8 @@
 from brainzutils.flask import CustomFlask
 import os
+from time import sleep
 
-
+RETRIES = 10
 def create_app(config_path=None):
     app = CustomFlask(
         import_name=__name__,
@@ -13,22 +14,27 @@ def create_app(config_path=None):
         os.path.dirname(os.path.realpath(__file__)),
         "..", "default_config.py"
     ))
-    app.config.from_pyfile(os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        '..', 'consul_config.py'
-    ), silent=True)
+    count = 0
+    while count < RETRIES:
+        app.config.from_pyfile(os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            '..', 'consul_config.py'
+        ), silent=True)
+        count += 1
+        sleep(1)
+
     app.config.from_pyfile(os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         '..', 'custom_config.py'
     ), silent=True)
     if config_path:
         app.config.from_pyfile(config_path)
-
     app.init_loggers(
         file_config=app.config.get("LOG_FILE"),
         sentry_config=app.config.get("LOG_SENTRY"),
     )
 
+    app.logger.error("Check Access Keys: {}".format(str(app.config['ACCESS_KEYS'])))
     # Blueprints
     from mbspotify.views import main_bp
     app.register_blueprint(main_bp)
