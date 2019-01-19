85d2f2ac5ed798ea35c30fd00391190e7ea17f03
code-master5 <lauhny.bimalk@gmail.com>
2019-01-20 03:14:23 +0530

    Wait for consul config

--- mbspotify/__init__.py
+++ mbspotify/__init__.py
@@ -1,8 +1,11 @@
 from brainzutils.flask import CustomFlask
 import os
+import sys
 from time import sleep
 
-RETRIES = 10
+deploy_env = os.environ.get('DEPLOY_ENV', '')
+CONSUL_CONFIG_FILE_RETRY_COUNT = 10
+
 def create_app(config_path=None):
     app = CustomFlask(
         import_name=__name__,
@@ -14,19 +17,29 @@ def create_app(config_path=None):
         os.path.dirname(os.path.realpath(__file__)),
         "..", "default_config.py"
     ))
-    count = 0
-    while count < RETRIES:
-        app.config.from_pyfile(os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            '..', 'consul_config.py'
-        ), silent=True)
-        count += 1
-        sleep(1)
 
     app.config.from_pyfile(os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         '..', 'custom_config.py'
     ), silent=True)
+
+    if deploy_env:
+        config_file = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            '..', 'consul_config.py'
+        )
+        print("Checking if consul template generated config file exists: {}".format(config_file))
+        for _ in range(CONSUL_CONFIG_FILE_RETRY_COUNT):
+            if not os.path.exists(config_file):
+                sleep(1)
+
+        if not os.path.exists(config_file):
+            print("No configuration file generated yet. Retried {} times, exiting.".format(CONSUL_CONFIG_FILE_RETRY_COUNT))
+            sys.exit(-1)
+
+        print("Loading consul config file {}".format(config_file))
+    app.config.from_pyfile(config_file, silent=True)
+
     if config_path:
         app.config.from_pyfile(config_path)
     app.init_loggers(
